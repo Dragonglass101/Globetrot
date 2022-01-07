bd4ac1084554a0631ccbb713345c18d3930e1b41
Anurag Singh Naruka <Anuragsinghnaruka@gmail.com>
2022-01-07 23:27:57 +0530

    Base project

---
+++ Globetrot/Globetrot/app/src/main/java/com/example/globetrot/Login.kt
@@ -0,0 +1,45 @@
+package com.example.globetrot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Login.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Login : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_login, container, false)
+        view.findViewById<TextView>(R.id.sign_up).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_login_to_register)
+        }
+        view.findViewById<Button>(R.id.login_button).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_login_to_homescreen)
+        }
+        return view
+
+    }
+}
+
---
+++ Globetrot/Globetrot/app/src/main/java/com/example/globetrot/Register.kt
@@ -0,0 +1,31 @@
+package com.example.globetrot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+
+class Register : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_register, container, false)
+        view.findViewById<TextView>(R.id.sign_in).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_register_to_login)
+        }
+        return view
+    }
+
+}
